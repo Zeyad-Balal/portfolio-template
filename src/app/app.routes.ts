aa99dd7d25df19cce97bc7122f29eebf4be1f111
Zeyad Hassan <zeyad.hassan.balal@gmail.com>
2025-03-25 11:11:39 +0200

    Add home and landing page components with styles and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'landing', pathMatch: 'full'},
+    {path: 'landing', component: LandingComponent , title: 'Landing'},
+    {path: 'home', component: HomePageComponent , title: 'Home'},
+];
