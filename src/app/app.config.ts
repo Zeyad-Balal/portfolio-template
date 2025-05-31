f771c1424d9dc21fc0f625b2d60a0783d0c4cd3b
Zeyad-Balal <zeyad.hassan.balal@gmail.com>
2025-05-31 12:27:10 +0300

    feat: add ngx-owl-carousel-o for enhanced carousel functionality and implement skills and partners sections in portfolio component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withViewTransitions()),
+    // importProvidersFrom(BrowserAnimationsModule),
+    provideAnimations(),
+  ],
 };
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 100,
+    navText: ['', ''],
+    autoplay: true, // Enable autoplay
+    autoplayTimeout: 1000, 
+    // autoplayHoverPause: true, // Pause on hover
+    // autoplaySpeed: 1000, // Transition speed
+    smartSpeed: 500, // Smart speed for smooth transitions
+    fluidSpeed: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: false,
+  };
 }
